d24a6baf47713d270c884bd3878f22a8d4ab0776
naildown <naildown@163.com>
2017-09-21 17:37:22 +0800

    chai-as-promise works

--- app.js
+++ app.js
@@ -4,31 +4,33 @@
  * Definition and settings
  */
 const {By, until, Condition, Key, Button} = require('selenium-webdriver'),
-      driver = require('./lib/driver').build('safari'),
+      driver = require('./lib/driver').build('cef'),
       elements = require('./lib/elements'),
-      test = require('selenium-webdriver/testing');
-
-const assert = require('assert'),
+      assert = require('assert'),
       chai = require('chai'),
-      expect = chai.expect,
-      should = chai.should();
+      chaiAsPromised = require("chai-as-promised")
+    
+chai.use(chaiAsPromised);
+let expect = chai.expect,
+    should = chai.should();
 
 // Define common tools
 function errLog(err) {
-    console.error(err.name + ' : ' + err.message);
+    //console.error(err.name + ' : ' + err.message);
+    console.error(err.name);
 }
 
 // Define basic steps.
 let steps = {
   login : function (username, password){
     // click OK in the Warning popup  
-    driver.wait(until.elementLocated(elements.login.btn_OK), 1000).click().catch(err => errLog(err))
+    driver.wait(until.elementLocated(elements.login.btn_OK), 1000).click().catch(errLog)
     // select Uaername/Password Login if needed.
-    driver.findElement(elements.login.btn_UserLogin).click().catch(err => errLog(err))
+    driver.findElement(elements.login.btn_UserLogin).click().catch(errLog)
     // input username and password, then login.
     driver.findElement(elements.login.input_Username).sendKeys(username)
     driver.findElement(elements.login.input_Password).sendKeys(password)
-    driver.findElement(elements.login.btn_Login).click()
+    driver.wait(until.elementLocated(elements.login.btn_Login), 1000).click().catch(errLog)
   }
 }
 
@@ -36,38 +38,30 @@ let steps = {
  * Add test cases and run test
  */
 describe('Logon', function() {
+  this.timeout(0)
 
-  before(function(done) {
-    driver.get('https://foo3.clinicomp.com/webframe/index.php/logon?ctrl=Cps')
-      .then(done())
-  })
-  after(function(done) {
-    done()
-    //driver.quit()
-      //.then(done())
+  before(function() {
+    return driver.get('https://foo3.clinicomp.com/webframe/index.php/logon?ctrl=Cps')
   })
 
-  it('Logon should failed', function() {
-    driver.navigate().refresh()
-    steps.login('afsad', '123123')
-    let el = driver.wait(until.elementsLocated(elements.login.dlg_LogonFailed), 5000)
-    return Promise.resolve(el)
-      .then(function(el) {
-        assert(el)
-      })
+  beforeEach(function() {
+    //driver.manage().window().setSize(1024, 960)
+    return driver.navigate().refresh()
+  })
+  
+  after(function() {
+    return driver.quit()
   })
 
-  it('Logon should successed', function() {
-    driver.navigate().refresh()
-    driver.get('https://foo3.clinicomp.com/webframe/index.php/logon?ctrl=Cps')
+  it(' should failed.', function() {
     steps.login('dstest', '123')
-    driver.wait(until.urlIs('https://foo3.clinicomp.com/webframe/index.php/cps'), 30000)
+    return driver.wait(until.elementsLocated(elements.login.dlg_LogonFailed), 5000)
+      .should.be.fulfilled;
   })
 
-  it('test03', function() {
-    var promise = Promise.resolve(1);
-    return promise.then(function (value) {
-        assert(value === 1);
-    });
+  it(' should successed.', function() {
+    steps.login('demo', '123')
+    return driver.wait(until.urlIs('https://foo3.clinicomp.com/webframe/index.php/Cps'), 15000)
+      .should.be.fulfilled;
   })
 })
