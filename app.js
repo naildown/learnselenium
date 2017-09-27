13022473e9f4091a7964f1d420aa853e50e87f5a
naildown <naildown@163.com>
2017-09-27 14:23:19 +0800

    2017-09-27

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
  * Definition and settings
  */
 const {By, until, Condition, Key, Button} = require('selenium-webdriver'),
-      driver = require('./lib/driver').build('chrome'),
+      driver = require('./lib/driver').build('cef'),
       elements = require('./lib/elements'),
       assert = require('assert'),
       chai = require('chai'),
@@ -23,43 +23,39 @@ function errLog(err) {
 let steps = {
     Login : function(username, password) {
         // click OK in the Warning popup    
-        driver.wait(until.elementLocated(elements.login.btn_OK), 1000).click().catch(errLog)
+        driver.wait(until.elementLocated(elements.login.btn_warningOK), 1000).click().catch(errLog)
         // select Uaername/Password Login if needed.
         driver.findElement(elements.login.btn_UserLogin).click().catch(errLog)
         // input username and password, then login.
         driver.findElement(elements.login.input_Username).sendKeys(username)
         driver.findElement(elements.login.input_Password).sendKeys(password)
-        driver.sleep(100);
-        driver.findElement(elements.login.btn_Login).click()
+        driver.wait(until.elementLocated(elements.login.btn_Login), 1000).click().catch(errLog)
     },
 
     Validate: function(authPromptType, password) {
-        driver.sleep(100)
-        if(authPromptType !== 0 || authPromptType !== 1) {
+        if(authPromptType != 0 && authPromptType != 1) {
+            console.log('authPromptType: ' + authPromptType)
             throw new Error('authPrompt type is error')
             return
         } else if(authPromptType === 0) {
-            driver.findElement(elements.validation.input_Password).sendKeys(password)
+            driver.wait(until.elementLocated(elements.validation.input_Password),2000).sendKeys(password)
         }
-        driver.findElement(elements.validation.btn_OK).click()
+        driver.wait(until.elementLocated(elements.validation.btn_OK), 3000).click()
     },
 
     SelectUnit : function(unitName) {
         let xp = "//tr[starts-with(@data-qtip,'" + unitName + "')]"
         driver.wait(until.elementLocated(By.xpath(xp)), 10000).click().catch(errLog)
-        driver.sleep(2000)
     },
 
     AdmitPat : function(mrn, lastName, firstName, billing) {
         driver.wait(until.elementLocated(elements.patCtrl.icon_Adt), 5000).click().catch(errLog)
         driver.wait(until.elementLocated(elements.patCtrl.menu_Admit), 1000).click().catch(errLog)
-        driver.sleep(1000)
-        driver.findElement(elements.patCtrl.admit_input('FMP')).sendKeys(mrn).catch(errLog)
-        driver.findElement(elements.patCtrl.admit_input('Pat Last Name')).sendKeys(mrn).catch(errLog)
-        driver.findElement(elements.patCtrl.admit_input('Pat First Name')).sendKeys(mrn).catch(errLog)
-        driver.findElement(elements.patCtrl.admit_input('Billing Num')).sendKeys(mrn).catch(errLog)
-        driver.sleep(100)
-        driver.findElement(elements.patCtrl.btn_Admit).click()
+        driver.wait(until.elementLocated(elements.patCtrl.admit_input('FMP')), 3000).sendKeys(mrn).catch(errLog)
+        driver.findElement(elements.patCtrl.admit_input('Pat Last Name')).sendKeys(lastName).catch(errLog)
+        driver.findElement(elements.patCtrl.admit_input('Pat First Name')).sendKeys(firstName).catch(errLog)
+        driver.findElement(elements.patCtrl.admit_input('Billing Num')).sendKeys(billing).catch(errLog)
+        driver.wait(until.elementLocated(elements.patCtrl.btn_Admit), 3000).click().catch(errLog)
     },
 }
 
@@ -71,6 +67,10 @@ describe('Test 01', function() {
 
     before(function() {
         return driver.get('https://foo3.clinicomp.com/webframe/index.php/Cps')
+    })
+
+    beforeEach(function() {
+        return driver.navigate().refresh()
     })
 
     after(function() {
@@ -90,12 +90,14 @@ describe('Test 01', function() {
     })
 
     it('Admit a new patient.', function() {
+        driver.wait(until.elementLocated(elements.patCtrl.grid_PatSelection))
         steps.SelectUnit('MSI')
-        steps.AdmitPat('dstest0925', 'dstest', '0925', 'dstest0925')
-        steps.validation(0, '123')
-        driver.wait(until.elementLocated(elements.prompt.lbl_AdmitOK), 5000).should.be.fulfilled
-        driver.findElement(elements.prompt.btn_OK).click()
-        return driver.wait(until.elementLocated(By.xpath("//span[text()='Cancel']")), 10000)
+        steps.AdmitPat('dstest0925f', 'dstest', '0925f', 'dstest0925f')
+        steps.Validate(0, '123')
+        driver.wait(until.elementLocated(elements.prompt.lbl_AdmitOK), 5000)
+            .should.be.fulfilled
+        driver.findElement(elements.prompt.btn_AdmitOK).click().catch(errLog)
+        return driver.wait(until.elementsLocated(elements.patCtrl.grid_Focused('dstest, 0925f')), 10000)
             .should.be.fulfilled
     })
 })
